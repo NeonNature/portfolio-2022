57e865d326342b352b8969e6da0b911aa3329ffd
NeonNature <mustachemaung@gmail.com>
2023-07-30 08:28:51 +0630

    feat: add rough skills page

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -3,75 +3,133 @@ import { useControls } from "leva";
 import "../styles/composition.scss";
 import { useSpring, animated } from "@react-spring/three";
 import { useThree, useFrame } from "@react-three/fiber";
-import { Text3D, Text } from "@react-three/drei";
+import {
+    Text3D,
+    Text,
+    Billboard,
+    TrackballControls,
+    Float,
+    PositionalAudio,
+} from "@react-three/drei";
 import * as THREE from "three";
 
-const Word = ({ children, ...props }) => {
+const SkillList = [
+    "HTML",
+    // CSS
+    "CSS",
+    "CSS in JS",
+    "CSS Modules",
+    "CSS Flexbox",
+    "CSS Grid",
+    "Bootstrap",
+    "Tailwind",
+    "Sass/Scss",
+    // JS related
+    "Rescript",
+    "React",
+    "Javascript",
+    "JQuery",
+    "Typescript",
+    "React Native",
+    "GatsbyJS",
+    "ThreeJS",
+    // Others
+    "Cypress",
+    "RESTful API",
+    "GraphQL",
+    "npm",
+    "git",
+    "Responsive Web Design",
+    "Accessibility",
+    "Cross-browser",
+];
+
+const Word = ({ ...props }) => {
     const color = new THREE.Color();
     const fontProps = {
         font: "/Inter-Bold.woff",
-        fontSize: 2.5,
+        fontSize: 1.5,
         letterSpacing: -0.05,
         lineHeight: 1,
         "material-toneMapped": false,
     };
     const ref = useRef();
     const [hovered, setHovered] = useState(false);
+    const [clicked, setClicked] = useState(false);
     const over = (e) => (e.stopPropagation(), setHovered(true));
     const out = () => setHovered(false);
-    // Change the mouse cursor on hover
     useEffect(() => {
         if (hovered) document.body.style.cursor = "pointer";
         return () => (document.body.style.cursor = "auto");
     }, [hovered]);
-    // Tie component to the render-loop
-    useFrame(({ camera }) => {
-        // Make text face the camera
+
+    useFrame(({ camera, clock }) => {
         ref.current.quaternion.copy(camera.quaternion);
-        // Animate font color
         ref.current.material.color.lerp(
-            color.set(hovered ? "#fa2720" : "white"),
+            color.set(hovered || clicked ? "#fa2720" : "white"),
             0.1
         );
+
+        const a = clock.getElapsedTime();
+
+        // ref.current.position.x =
+        //     ref.current.position.x + 0.1 * Math.sin(2 * Math.PI * 0.25 * a);
     });
+
+    const onClick = () => {
+        setClicked(true);
+    };
+
     return (
-        <Text
-            ref={ref}
-            onPointerOver={over}
-            onPointerOut={out}
-            onClick={() => console.log("clicked")}
-            {...props}
-            {...fontProps}
-            children={children}
-        />
+        <Float>
+            <Text
+                ref={ref}
+                onPointerOver={over}
+                onPointerOut={out}
+                onClick={onClick}
+                {...props}
+                {...fontProps}
+            >
+                {props.text}
+            </Text>
+
+            <group position={[0, 0, 0]}>
+                {hovered && (
+                    <PositionalAudio
+                        url={"/static/media/sound1.wav"}
+                        distance={100}
+                        loop
+                    />
+                )}
+            </group>
+        </Float>
     );
 };
 
-const SkillWord = ({ count = 4, radius = 20 }) => {
-    const words = useMemo(() => {
-        const temp = [];
-        const spherical = new THREE.Spherical();
-        const phiSpan = Math.PI / (count + 1);
-        const thetaSpan = (Math.PI * 2) / count;
-        for (let i = 1; i < count + 1; i++)
-            for (let j = 0; j < count; j++)
-                temp.push([
-                    new THREE.Vector3().setFromSpherical(
-                        spherical.set(radius, phiSpan * i, thetaSpan * j)
-                    ),
-                    "React",
-                ]);
-        return temp;
-    }, [count, radius]);
-    return words.map(([pos, word], index) => (
-        <Word key={index} position={pos} children={word} />
+const SkillWord = () => {
+    const { width, height } = useThree((state) => state.viewport);
+
+    const randomIntFromInterval = (max, min) => {
+        return Math.floor(Math.random() * (max - min + 1) + min);
+    };
+
+    return SkillList.map((val, index) => (
+        <Word
+            key={index}
+            position={[
+                randomIntFromInterval(width / 2 - 3, -width / 2 + 3),
+                randomIntFromInterval(height / 2 - 3, -height / 2 + 3),
+                0,
+            ]}
+            text={val}
+        />
     ));
 };
 
 const Skills = ({ isActive }) => {
     const { width, height } = useThree((state) => state.viewport);
 
-    const mesh = useRef();
+    const ref = useRef();
 
     const { position, rotation } = useSpring({
         position: isActive
@@ -82,12 +140,8 @@ const Skills = ({ isActive }) => {
 
     return (
         <>
-            <animated.group
-                position={position}
-                rotation={rotation}
-                className="nav-icon"
-            >
-                <SkillWord count={8} radius={20} />
+            <animated.group position={position} ref={ref}>
+                <SkillWord />
             </animated.group>
         </>
     );
