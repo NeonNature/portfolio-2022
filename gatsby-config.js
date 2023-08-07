fc53cb19c1be62727ca88f12edaf8f24a94d306f
NeonNature <mustachemaung@gmail.com>
2023-08-07 09:02:03 +0630

    chore: add GA and prepare for seo

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,24 +1,33 @@
 module.exports = {
-  siteMetadata: {
-    title: `Portfolio 2022`,
-    siteUrl: `https://www.yourdomain.tld`
-  },
-  plugins: ["gatsby-plugin-sass", {
-    resolve: 'gatsby-plugin-google-analytics',
-    options: {
-      "trackingId": "placeholderId"
-    }
-  }, "gatsby-plugin-image", "gatsby-plugin-sitemap", {
-    resolve: 'gatsby-plugin-manifest',
-    options: {
-      "icon": "src/images/icon.png"
-    }
-  }, "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
+    siteMetadata: {
+        title: `Neon - Tech Voyager`,
+        siteUrl: `https://voyager-neon.tech/`,
     },
-    __key: "images"
-  }]
+    plugins: [
+        "gatsby-plugin-sass",
+        "gatsby-plugin-image",
+        "gatsby-plugin-sitemap",
+        {
+            resolve: `gatsby-plugin-google-gtag`,
+            options: {
+                trackingIds: ["G-9HK0TS22D8"],
+            },
+        },
+        {
+            resolve: "gatsby-plugin-manifest",
+            options: {
+                icon: "src/images/icon.png",
+            },
+        },
+        "gatsby-plugin-sharp",
+        "gatsby-transformer-sharp",
+        {
+            resolve: "gatsby-source-filesystem",
+            options: {
+                name: "images",
+                path: "./src/images/",
+            },
+            __key: "images",
+        },
+    ],
 };
